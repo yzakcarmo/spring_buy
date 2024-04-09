e58bffec732a6b43870a9972fc94e8b1f6aa12a8
Yzak Carmo <yzak.carmo@gmail.com>
2024-04-09 18:10:30 -0300

    first commit

---
+++ src/main/java/com/yzak/spring_buy/SpringBuyApplication.java
@@ -0,0 +1,13 @@
+package com.yzak.spring_buy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yzak/spring_buy/SpringBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzak.spring_buy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
